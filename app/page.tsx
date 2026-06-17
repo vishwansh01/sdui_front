5f106dede8e148a4fc5017ca04735b6074df41d6
Vishwansh Kumar <kumarvishwansh@gmail.com>
2026-06-18 02:10:40 +0530

    added Layout1

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,292 @@
-import Image from "next/image";
+"use client";
+
+import ContentBox from "@/components/ContentBox";
+import NavBar from "@/components/NavBar";
+const components = {
+  nav: NavBar,
+  contentBox: ContentBox,
+};
+const jsonLayout1 = {
+  screenName: "home",
+  version: 1,
+  components: [
+    {
+      compName: "NAV",
+      version: 1,
+      type: "nav",
+      props: {
+        title: "This is Nav bar",
+      },
+      cssProps: "row-span-1 col-span-9 text-center",
+    },
+    // {
+    //   compName: "Middle",
+    //   version: 1,
+    //   type: "contentBox",
+    //   props: {},
+    //   cssProps: "col-span-9 row-span-4",
+    //   components: [
+
+    {
+      compName: "Left_Section",
+      version: 1,
+      type: "contentBox",
+      props: {},
+      cssProps: "row-span-4 col-span-3 text-center overflow-y-scroll",
+      components: [
+        {
+          compName: "div1",
+          type: "contentBox",
+          props: {
+            content: "div1",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div2",
+          type: "contentBox",
+          props: {
+            content: "div2",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div3",
+          type: "contentBox",
+          props: {
+            content: "div3",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div4",
+          type: "contentBox",
+          props: {
+            content: "div4",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div5",
+          type: "contentBox",
+          props: {
+            content: "div5",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div6",
+          type: "contentBox",
+          props: {
+            content: "div6",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div7",
+          type: "contentBox",
+          props: {
+            content: "div7",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div8",
+          type: "contentBox",
+          props: {
+            content: "div8",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div9",
+          type: "contentBox",
+          props: {
+            content: "div9",
+          },
+          cssProps: "h-1/2",
+        },
+      ],
+    },
+    {
+      compName: "Hero",
+      version: 1,
+      type: "contentBox",
+      props: {
+        title: "Title",
+        content: "Content",
+      },
+      cssProps:
+        "flex items-center flex-col justify-center col-span-3 row-span-4 text-center",
+    },
+    {
+      compName: "Right_Section",
+      version: 1,
+      type: "contentBox",
+      props: {},
+      cssProps: " row-span-4 col-span-3 text-center overflow-y-hidden",
+      components: [
+        {
+          compName: "div1",
+          type: "contentBox",
+          props: {
+            content: "div1",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div2",
+          type: "contentBox",
+          props: {
+            content: "div2",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div3",
+          type: "contentBox",
+          props: {
+            content: "div3",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div4",
+          type: "contentBox",
+          props: {
+            content: "div4",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div5",
+          type: "contentBox",
+          props: {
+            content: "div5",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div6",
+          type: "contentBox",
+          props: {
+            content: "div6",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div7",
+          type: "contentBox",
+          props: {
+            content: "div7",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div8",
+          type: "contentBox",
+          props: {
+            content: "div8",
+          },
+          cssProps: "h-1/2",
+        },
+        {
+          compName: "div9",
+          type: "contentBox",
+          props: {
+            content: "div9",
+          },
+          cssProps: "h-1/2",
+        },
+      ],
+    },
+    //   ],
+    // },
+    {
+      compName: "Footer",
+      version: 1,
+      type: "contentBox",
+      props: {
+        title: "Footer",
+      },
+      cssProps: "col-span-9 text-center row-span-1",
+    },
+  ],
+  props: {},
+  cssProps:
+    "grid text-white h-screen bg-[linear-gradient(to_bottom,hsl(220,_65%,_5%)_0%,hsl(220,_65%,_3.52%)_50%,hsl(220,_65%,_10%)_100%)]",
+};
+const jsonLayout2 = {};
+
+type CompProps = {
+  compName: string;
+  version?: number;
+  type: string;
+  props: {
+    title?: string;
+    content?: string;
+  };
+  cssProps: string;
+  components?: CompProps[];
+};
+
+function RenderComponent(comp: CompProps) {
+  const Component = components[comp.type as keyof typeof components];
+  if (!Component) {
+    return null;
+  }
+  return (
+    <Component {...comp}>
+      {comp.components &&
+        comp.components.map((child) => {
+          return <RenderComponent {...child} key={child.compName} />;
+        })}
+    </Component>
+  );
+}
+// json.components.map(RenderComponent);
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="grid text-white h-screen bg-[linear-gradient(to_bottom,hsl(220,_65%,_5%)_0%,hsl(220,_65%,_3.52%)_50%,hsl(220,_65%,_10%)_100%)]">
+      {jsonLayout1.components.map((comp) => {
+        return <RenderComponent {...comp} key={comp.compName} />;
+      })}
+      {/* {json.screenName == "home" &&
+        json.components.map((comp) => {
+          if (comp.type == "contentBox") {
+            return <ContentBox {...comp} key={comp.compName} />;
+          }
+          if (comp.type == "nav") {
+            return <NavBar {...comp} key={comp.compName} />;
+          }
+        })} */}
+      {/* <div className="col-span-3 text-center">NAV</div> */}
+      {/* <div className="row-span-2 text-center overflow-y-scroll"> */}
+      {/* <div className="h-1/2">0</div>
+        <div className="h-1/2">1</div>
+        <div className="h-1/2">2</div>
+        <div className="h-1/2">3</div>
+        <div className="h-1/2">4</div>
+        <div className="h-1/2">5</div>
+        <div className="h-1/2">9</div>
+        <div className="h-1/2">6</div>
+        <div className="h-1/2">7</div>
+        <div className="h-1/2">8</div> */}
+      {/* </div> */}
+      {/* // <div className="row-span-2 text-center">Center</div> */}
+      {/* // <div className="row-span-2 text-center overflow-hidden"> */}
+      {/* <div className="h-1/2">0</div>
+        <div className="h-1/2">1</div>
+        <div className="h-1/2">2</div>
+        <div className="h-1/2">3</div>
+        <div className="h-1/2">4</div>
+        <div className="h-1/2">5</div>
+        <div className="h-1/2">9</div>
+        <div className="h-1/2">6</div>
+        <div className="h-1/2">7</div>
+        <div className="h-1/2">8</div> */}
+      {/* // </div> */}
+    </main>
   );
 }
